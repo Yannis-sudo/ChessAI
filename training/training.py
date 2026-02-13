6016e141a87726bcfb5bc7697980102f2d306bce
Yannis-sudo <ya10be@gmx.de>
2026-02-13 21:08:34 +0100

    Position API Download Added

--- training/training.py
+++ training/training.py
@@ -1,5 +1,13 @@
 import torch
 import torch.nn as nn
+from random import randint
+
+from get_games import get_games, get_next_player_month, players, update_next_player_month
 
 def train_loop(model):
-    pass
+    random_player_id = randint(0, len(players) - 1)
+    next_player = get_next_player_month(players[random_player_id])
+    positions = get_games(100, random_player_id, next_player["year"], next_player["month"])
+    update_next_player_month(players[random_player_id])
+
+    
